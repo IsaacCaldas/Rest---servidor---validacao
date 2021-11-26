eb306b5e6fe7ec7e89732708f2a62872d2022be7
Isaac Caldas <marcos.felipe.clear@gmail.com>
2021-11-26 13:27:39 -0300

    submodule update

--- utils/validator.js
+++ utils/validator.js
@@ -2,9 +2,9 @@ module.exports = {
 
     user:(app, req, res) => {
         
-        req.assert('name', 'O nome é obrigatório!').notEmpty();
-        req.assert('email', 'O e-mail está inválido.').notEmpty().isEmail();
-        req.assert('password', 'A senha é obrigatório!').notEmpty();
+        req.assert('_name', 'O nome é obrigatório!').notEmpty();
+        req.assert('_email', 'O e-mail está inválido.').notEmpty().isEmail();
+        req.assert('_password', 'A senha é obrigatório!').notEmpty();
 
         let errors = req.validationErrors();
 
